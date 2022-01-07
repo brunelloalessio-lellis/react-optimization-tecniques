379134081dcb785e31d4bd738898423462f5a5c7
abrunello <brunello@bludata.com>
2022-01-07 15:26:59 +0100

    rendering component concept

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 
 import "./App.css";
+import DemoOutput from "./components/Demo/Demo";
 import Button from "./components/UI/Button/Button";
 
 function App() {
@@ -17,7 +18,7 @@ function App() {
   return (
     <div className="app">
       <h1>Hi there!</h1>
-      {showParagraph && <p>This is new!</p>}
+      <DemoOutput show={showParagraph}/>
       <Button onClick={toggleParagraphHandler}>Show paragraph</Button>
     </div>
   );
